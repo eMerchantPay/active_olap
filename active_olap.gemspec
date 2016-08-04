5ffb006be7ba13e49612be045a1660958b44c933
Emil Petkov <emil@emerchantpay.com>
2016-08-04 12:45:34 +0300

    Added support for Rails 4.x

--- active_olap.gemspec
+++ active_olap.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name    = 'active_olap'
-  
+
   # Do not update version and date by hand: this will be done automatically.
-  s.version = "0.0.7"
+  s.version = "0.0.8"
   s.date    = "2010-01-29"
-  
+
   s.summary     = "Extend ActiveRecord with OLAP query functionality."
   s.description = "Extends ActiveRecord with functionality to perform OLAP queries on your data. Includes helper method to ease displaying the results."
-  
+
   s.authors  = ['Willem van Bergen']
   s.email    = ['willem@vanbergen.org']
   s.homepage = 'http://wiki.github.com/wvanbergen/active_olap'
-  
+
   # Do not update files and test_files by hand: this will be done automatically.
   s.files      = %w(spec/unit/cube_spec.rb spec/spec_helper.rb lib/active_olap/configurator.rb test/helper_modules_test.rb .gitignore test/active_olap_test.rb lib/active_olap/helpers/table_helper.rb lib/active_olap/helpers/display_helper.rb lib/active_olap/dimension.rb active_olap.gemspec MIT-LICENSE lib/active_olap/category.rb lib/active_olap.rb init.rb Rakefile spec/integration/active_olap_spec.rb lib/active_olap/test/assertions.rb README.rdoc tasks/github-gem.rake lib/active_olap/helpers/form_helper.rb lib/active_olap/helpers/chart_helper.rb test/helper.rb lib/active_olap/cube.rb lib/active_olap/aggregate.rb)
   s.test_files = %w(spec/unit/cube_spec.rb test/helper_modules_test.rb test/active_olap_test.rb spec/integration/active_olap_spec.rb)
--- lib/active_olap.rb
+++ lib/active_olap.rb
@@ -1,38 +1,38 @@
 module ActiveOLAP
-  
+
   def enable_active_olap(config = nil, &block)
 
     self.send(:extend, ClassMethods)
     self.send(:extend, Utils)
-    
-    self.scope :olap_drilldown, lambda { |hash| self.olap_drilldown_finder_options(hash) }    
-    
+
+    self.scope :olap_drilldown, lambda { |hash| self.olap_drilldown_finder_options(hash) }
+
     self.cattr_accessor :active_olap_dimensions, :active_olap_aggregates
     self.active_olap_dimensions = {}
-    self.active_olap_aggregates = {}    
-    
+    self.active_olap_aggregates = {}
+
     if config.nil? && block_given?
       conf = Configurator.new(self)
-      yield(conf) 
+      yield(conf)
     end
-    
+
   end
-  
-  
+
+
   module ClassMethods
-    
+
     # Performs an OLAP query that counts how many records do occur in given categories.
-    # It can be used for multiple dimensions 
+    # It can be used for multiple dimensions
     # It expects a list of category definitions
     def olap_query(*args)
 
       # set aggregates apart if they are given
       aggregates_given = (args.last.kind_of?(Hash) && args.last.has_key?(:aggregate)) ? args.pop[:aggregate] : nil
-      
+
       # parse the dimensions
-      raise "You have to provide at least one dimension for an OLAP query" if args.length == 0    
+      raise "You have to provide at least one dimension for an OLAP query" if args.length == 0
       dimensions = args.collect { |d| Dimension.create(self, d) }
-      
+
       raise "Overlapping categories only supported in the last dimension" if dimensions[0..-2].any? { |d| d.has_overlap? }
       raise "Only counting is supported with overlapping categories" if dimensions.last.has_overlap? && aggregates_given
 
@@ -42,8 +42,8 @@ module ActiveOLAP
         else
           aggregates = [Aggregate.create(self, :the_olap_count_field, :count_distinct)]
         end
-      else 
-        aggregates = Aggregate.all_from_olap_query_call(self, aggregates_given)        
+      else
+        aggregates = Aggregate.all_from_olap_query_call(self, aggregates_given)
       end
 
       conditions = self.send(:merge_conditions, *dimensions.map(&:conditions))
@@ -55,41 +55,44 @@ module ActiveOLAP
 
       if aggregates.length > 0
         dimensions_to_group = dimensions.clone
-      else 
+      else
         selects << dimensions.last.to_count_with_overlap_sql
         dimensions_to_group = dimensions[0, dimensions.length - 1]
       end
-      
+
       dimensions_to_group.each_with_index do |d, index|
         var_name = "dimension_#{index}"
         groups  << self.connection.quote_column_name(var_name)
         selects << d.to_case_expression(var_name)
       end
-    
+
       group_clause = groups.length > 0 ? groups.join(', ') : nil
       # TODO: having
-      
+
       olap_temporarily_set_join_type if joins_clause
-      
-      query_result = self.scoped(:conditions => conditions).find(:all, :select => selects.join(', '), 
-          :joins => joins_clause, :group => group_clause, :order => group_clause)  
-      
+
+      result = self.where(conditions)
+      result = result.select(selects.join(', ')) if selects.present?
+      result = result.joins(joins_clause) if joins_clause.present?
+      result = result.group(group_clause) if group_clause.present?
+      result = result.order(group_clause) if group_clause.present?
+
       olap_temporarily_reset_join_type if joins_clause
-      
-      return Cube.new(self, dimensions, aggregates, query_result)
-    end   
+
+      return Cube.new(self, dimensions, aggregates, result)
+    end
   end
-  
+
   protected
-  
+
   def olap_drilldown_finder_options(options)
-    raise "You have to provide at least one dimension for an OLAP query" if options.length == 0    
+    raise "You have to provide at least one dimension for an OLAP query" if options.length == 0
 
     # returns an options hash to create a scope (the named_scope :olap_drilldown)
     conditions = options.map { |dim, cat| Dimension.create(self, dim).sanitized_sql_for(cat) }
-    { :select => connection.quote_table_name(table_name) + '.*', :conditions => self.send(:merge_conditions, *conditions) }
+    { select: connection.quote_table_name(table_name) + '.*', conditions: self.send(:merge_conditions, *conditions) }
   end
-  
+
   # temporarily use LEFT JOINs for specified :joins
   def olap_temporarily_set_join_type
     ActiveRecord::Associations::ClassMethods::InnerJoinDependency::InnerJoinAssociation.send(:define_method, :join_type) { "LEFT OUTER JOIN" }
@@ -99,7 +102,7 @@ module ActiveOLAP
   def olap_temporarily_reset_join_type
     ActiveRecord::Associations::ClassMethods::InnerJoinDependency::InnerJoinAssociation.send(:define_method, :join_type) { "INNER JOIN" }
   end
-  
+
 end
 
 require "active_olap/utils"
